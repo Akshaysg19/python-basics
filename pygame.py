c4ae6642a1a004e073fa3230cfcd46d57cf4bb90
Akshaysg19 <akshaysgujjar@gmail.com>
2023-02-09 14:41:00 +0530

    Adding SQL code

--- pygame.py
+++ pygame.py
@@ -3,7 +3,9 @@ import mysql.connector
 mydb = mysql.connector.connect(host='localhost', user='root', password='root', database='world')
 mycursor = mydb.cursor()
 #mycursor.execute('select * from city')
-mycursor.execute('select Name, CountryCode from city')
+#mycursor.execute('select Name, CountryCode from city')
+#mycursor.execute('select * from city order by CountryCode')
+mycursor.execute('select * from city order by CountryCode desc')
 myresult = mycursor.fetchall()
 
 for x in myresult:
